a670a6ea3bda57c33eb72f2745680f520c62f303
Andrii <farikpite@gmail.com>
2019-10-30 22:07:38 +0200

    Updates

---
+++ precache-manifest.b2cf6044518a1f17097e0568f8c83597.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db6ed5517e361feac96704bb30aaadb",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "d7b3e0a78bd2d31fb03c",
+    "url": "/react_add-todo-form/static/css/main.d79d44f5.chunk.css"
+  },
+  {
+    "revision": "b6a5a9b3de4eb62e6076",
+    "url": "/react_add-todo-form/static/js/2.f632bb41.chunk.js"
+  },
+  {
+    "revision": "d7b3e0a78bd2d31fb03c",
+    "url": "/react_add-todo-form/static/js/main.b45b7038.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
